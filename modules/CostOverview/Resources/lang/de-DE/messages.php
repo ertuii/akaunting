927e9948ab1a46e2648f88f0c5abf10da3724086
Arthur <86535916+ertuii@users.noreply.github.com>
2026-01-13 20:12:22 +0100

    Delete modules/CostOverview directory

--- modules/CostOverview/Resources/lang/de-DE/messages.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'email_sent' => 'Kostenübersicht wurde per E-Mail an :customer gesendet.',
-    'pdf_generated' => 'PDF-Kostenübersicht wurde erstellt.',
-];
--- modules/CostOverview/Resources/lang/en-GB/messages.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'email_sent' => 'Cost overview has been sent via email to :customer.',
-    'pdf_generated' => 'Cost overview PDF has been generated.',
-];
